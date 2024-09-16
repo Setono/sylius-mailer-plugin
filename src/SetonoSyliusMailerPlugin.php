795f4561ca605a1b71ac15ea2f7816c28cf27ef7
Joachim Løvgaard <joachim@loevgaard.dk>
2024-09-16 11:32:48 +0200

    Rename files

--- src/SetonoSyliusMailerPlugin.php
+++ src/SetonoSyliusMailerPlugin.php
@@ -2,12 +2,12 @@
 
 declare(strict_types=1);
 
-namespace Acme\SyliusExamplePlugin;
+namespace Setono\SyliusMailerPlugin;
 
 use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-final class AcmeSyliusExamplePlugin extends Bundle
+final class SetonoSyliusMailerPlugin extends Bundle
 {
     use SyliusPluginTrait;
 }
