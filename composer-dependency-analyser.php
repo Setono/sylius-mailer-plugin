0dac595c5411a1235f13c58a7f7e785308526951
Joachim Løvgaard <joachim@loevgaard.dk>
2024-09-23 11:17:02 +0200

    Fix deps

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -1,7 +1,16 @@
 <?php
 
 use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
+use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
 
 return (new Configuration())
     ->addPathToExclude(__DIR__ . '/tests')
+    ->ignoreErrorsOnPackage('swiftmailer/swiftmailer', [ErrorType::SHADOW_DEPENDENCY])
+    ->ignoreErrorsOnPackage('symfony/mime', [ErrorType::SHADOW_DEPENDENCY])
+    ->ignoreErrorsOnPackage('symfony/twig-bridge', [ErrorType::SHADOW_DEPENDENCY])
+    ->ignoreUnknownClasses([
+        Symfony\Component\Mailer\Event\MessageEvent::class,
+        Symfony\Component\Mailer\Event\SentMessageEvent::class,
+        Symfony\Component\Mailer\MailerInterface::class,
+    ])
 ;
