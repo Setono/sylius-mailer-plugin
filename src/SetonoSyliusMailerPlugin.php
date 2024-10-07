238d2a3ee9d7dbd90c1b70aa3e66c119874ee73c
Joachim Løvgaard <joachim@loevgaard.dk>
2024-10-07 13:15:52 +0200

    Load the compiler pass

--- src/SetonoSyliusMailerPlugin.php
+++ src/SetonoSyliusMailerPlugin.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace Setono\SyliusMailerPlugin;
 
+use Setono\SyliusMailerPlugin\DependencyInjection\Compiler\RegisterSwiftMailerListenerPass;
 use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
 use Sylius\Bundle\ResourceBundle\AbstractResourceBundle;
 use Sylius\Bundle\ResourceBundle\SyliusResourceBundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 final class SetonoSyliusMailerPlugin extends AbstractResourceBundle
 {
@@ -17,5 +19,12 @@ final class SetonoSyliusMailerPlugin extends AbstractResourceBundle
         return [
             SyliusResourceBundle::DRIVER_DOCTRINE_ORM,
         ];
+    }
+
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new RegisterSwiftMailerListenerPass());
     }
 }
